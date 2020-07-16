4e9cd58a750761c27275a2a81a486975c891e8d6
Stephen Studer <stephenkstuder@gmail.com>
2020-07-15 21:45:55 -0700

    attach logic completed

--- app.js
+++ app.js
@@ -11,5 +11,38 @@ new Vue({
       this.playerHealth = 100;
       this.monsterHealth = 100;
     },
+    attack() {
+      this.monsterHealth -= this.calculateDamage(3, 10);
+      if (this.checkWin()) {
+        return;
+      }
+
+      this.playerHealth -= this.calculateDamage(5, 12);
+      this.checkWin();
+    },
+    specialAttack() {},
+    heal() {},
+    giveUp() {},
+    calculateDamage(min, max) {
+      return Math.max(Math.floor(Math.random() * max) + 1, min);
+    },
+    checkWin() {
+      if (this.monsterHealth <= 0) {
+        if (confirm("You Won! New Game?")) {
+          this.startGame();
+        } else {
+          this.gameIsRunning = false;
+        }
+        return true;
+      } else if (this.playerHealth <= 0) {
+        if (confirm("You Lost... New Game?")) {
+          this.startGame();
+        } else {
+          this.gameIsRunning = false;
+        }
+        return true;
+      }
+      return false;
+    },
   },
 });
