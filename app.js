321df617fd22f11eae608e496153b9bf60209c36
Stephen Studer <stephenkstuder@gmail.com>
2020-07-19 18:42:36 -0700

    basic game functionality completed

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ new Vue({
       this.gameIsRunning = true;
       this.playerHealth = 100;
       this.monsterHealth = 100;
+      this.turns = [];
     },
     attack() {
       let damage = this.calculateDamage(3, 10);
@@ -26,7 +27,12 @@ new Vue({
       this.monsterAttacks();
     },
     specialAttack() {
-      this.monsterHealth -= this.calculateDamage(10, 20);
+      let damage = this.calculateDamage(10, 20);
+      this.monsterHealth -= damage;
+      this.turns.unshift({
+        isPlayer: true,
+        text: "Player Hits Monster HARD for " + damage,
+      });
       if (this.checkWin()) {
         return;
       }
@@ -35,6 +41,10 @@ new Vue({
     },
     heal() {
       this.playerHealth <= 90 ? (this.playerHealth += 10) : (this.playerHealth = 100);
+      this.turns.unshift({
+        isPlayer: true,
+        text: "Player heals for 10",
+      });
 
       this.monsterAttacks();
     },
